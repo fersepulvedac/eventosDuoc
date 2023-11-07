b4e74a453ca5e9430eadaae7596edf95f08bd102
Fernando Sepulveda <fer.sepulvedac@gmail.com>
2023-11-07 14:46:08 -0300

    antes de formatear

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,7 @@ import { ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild } f
 import { CalEvent, EventsService } from '../services/events.service';
 import { format } from 'date-fns';
 import { CalendarComponent, CalendarMode } from 'ionic2-calendar';
-import { AnimationController, Platform, ToastController } from '@ionic/angular';
+import { AlertController, AnimationController, Platform, ToastController } from '@ionic/angular';
 import { es } from 'date-fns/esm/locale'
 import { ScheduleOptions, LocalNotifications } from '@capacitor/local-notifications';
 import { Calendar } from '@awesome-cordova-plugins/calendar/ngx';
@@ -15,9 +15,10 @@ import { Calendar } from '@awesome-cordova-plugins/calendar/ngx';
 })
 export class HomePage {
 
+  isEditingEvent = false
   evento: any = {}
   isEventOpen = false
-  isOpcionesModo=false
+  isOpcionesModo = false
   admin = false
   fechaSeleccionada = ""
   isModalOpen = false;
@@ -25,15 +26,19 @@ export class HomePage {
   showTermino = false;
   usuario = "invitado"
 
+  numerosRomanos = [
+    'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX'
+  ];
+
   @ViewChild(CalendarComponent) myCal!: CalendarComponent
-  eventsSource: any[] = []
-  eventos: any[] = []
+  eventsSource: any[] = [] // LOS DEL CALENDARIO
+  eventos: any[] = [] //LOS MISMOS PERO ORDENADOS ALL DAY PRIMERO
   calendar = {
     mode: 'month' as CalendarMode,
     currentDate: new Date(),
     locale: 'es-CL',
     noEventsLabel: 'No hay eventos este día',
-    allDayLabel: "Todo el dia"
+    allDayLabel: "Todo el dia",
   }
 
   swipeEvent(e: any) {
@@ -43,19 +48,26 @@ export class HomePage {
     }
   }
 
-  toggleOpcionesModo(valor:boolean){
+  mostrandoOpcionesVista = false
+
+  toggleOpcionesModo() {
+    let valor = !this.mostrandoOpcionesVista
+    let op = "calc(((100vw - 7.5rem)/6) - 0.25rem)"
+    let cantidad = "calc(6rem + 30px + 8vh)"
     this.anim.create().addElement(document.querySelector("#opciones-modo")!)
-    .duration(300)
-    .fromTo("opacity", 0, 1)
-    .fromTo("transform", "translateX("+(valor?'-60px':'0px')+")", "translateX("+(valor?'0px':'-60px')+")")
-    .play()
-    this.anim.create().addElement(document.querySelectorAll("#opciones-modo>ion-icon")!)
-    .duration(300)
-    .fromTo("transform", "rotate("+(valor?"90deg":"0deg")+")", "rotate("+(valor?"0deg":"90deg")+")")
-    .play()
+      .duration(400)
+      .fromTo("opacity", `${valor ? 0 : 1}`, `${valor ? 1 : 0}`)
+      .fromTo("transform", `translateX(${op}) translateY(${valor ? cantidad : '0px'})`, `translateX(${op}) translateY(${valor ? '0px' : cantidad})`)
+      .play()
+    this.anim.create().addElement(document.querySelectorAll("#opciones-modo>.icon-opciones-modo")!)
+      .duration(500)
+      .fromTo("transform", `rotate(${valor ? '-90deg' : '0deg'})`, `rotate(${valor ? '0deg' : '-90deg'})`)
+      .play()
+    this.mostrandoOpcionesVista = valor
   }
 
   newEvent: any = {
+    id: -1,
     title: "",
     author: "Invitado",
     startTime: null,
@@ -88,7 +100,8 @@ export class HomePage {
     private zone: NgZone,
     private cdr: ChangeDetectorRef,
     private calendario: Calendar,
-    private toast: ToastController
+    private toast: ToastController,
+    private alert: AlertController
   ) {
     if (!localStorage.getItem("tema")) {
       localStorage.setItem("tema", "claro")
@@ -106,6 +119,15 @@ export class HomePage {
 
   agregarACalendario(evento: any) {
     this.zone.run(() => {
+      let termino = new Date(evento.endTime)
+      if (evento.allDay) {
+        termino.setHours(23)
+        termino.setMinutes(59)
+      }
+      if (termino < new Date()) {
+        this.mostrarToast("Este eventó ya terminó!.")
+        return
+      }
       if (evento.notify) {
         this.calendario.deleteEvent(
           evento.title, // title
@@ -124,12 +146,12 @@ export class HomePage {
         );
       }
 
-      this.eventos.forEach((e) => {
+      this.eventsSource.forEach((e) => {
         if (e.id == evento.id) {
           e.notify = !e.notify
         }
       })
-      this.events.setData(this.eventos)
+      this.events.setData(this.eventsSource)
 
       this.anim.create()
         .addElement(document.querySelector("#notificar" + evento.id)!)
@@ -150,16 +172,6 @@ export class HomePage {
     })
   }
 
-  ngAfterViewInit() {
-    this.animaSplash()
-    this.platform.ready().then(async () => {
-
-      this.eventsSource = await this.events.getData()
-
-      this.cdr.detectChanges();
-    });
-  }
-
   ionViewDidEnter() {
     if (localStorage.getItem("usuario")) {
       this.newEvent.author = JSON.parse(localStorage.getItem("usuario")!)["nombre"]
@@ -169,32 +181,52 @@ export class HomePage {
     }
   }
 
-  cerrarModal() {
-    this.isModalOpen = false
+  ngAfterViewInit() {
+    this.animaSplash()
+    this.platform.ready().then(async () => {
+
+      this.eventsSource = await this.events.getData()
+      if (this.calendar.mode == 'month') {
+        this.cdr.detectChanges();
+      }
+    });
   }
 
-  cambiaVista(valor: string) {
-    this.calendar.mode = valor as CalendarMode
-    if(valor == 'day' || valor=='week'){
-      document.querySelector(".eventos-border")?.classList.add("oculto")
-      document.querySelector(".eventos-border")?.classList.remove("visible")
-      document.querySelector(".calendar-container")?.classList.add("full")
-      document.querySelector(".calendar-container")?.classList.remove("middle")
-    }else{
-      document.querySelector(".eventos-border")?.classList.add("visible")
-      document.querySelector(".eventos-border")?.classList.remove("oculto")
-      document.querySelector(".calendar-container")?.classList.add("middle")
-      document.querySelector(".calendar-container")?.classList.remove("full")
-    }  
-    this.toggleOpcionesModo(false)
+  async pregunta(texto: string, action: () => void) {
+    const alert = await this.alert.create({
+      header: 'Confirmar',
+      // subHeader: texto,
+      message: texto,
+      buttons: [{
+        text: "Si",
+        handler: action
+      },
+      {
+        text: "No"
+      }],
+    });
+    await alert.present();
   }
 
-  sheduleEvent() {
+
+  async sheduleEvent() {
+    if(this.newEvent.title.length == 0){
+      document.querySelector("#input-titulo")!.classList.add("incompleto")
+      return
+    }
+    let inicio = new Date(this.newEvent.startTime)
+    let final = new Date(this.newEvent.endTime)
+    if (this.newEvent.allDay) {
+      inicio.setHours(8);
+      inicio.setMinutes(0);
+      final.setHours(23);
+      final.setMinutes(0);
+    }
     const toAdd: CalEvent = {
-      id: this.eventos.length + 1,
+      id: this.eventsSource.length + 1,
       title: this.newEvent.title,
-      startTime: new Date(this.newEvent.startTime),
-      endTime: new Date(this.newEvent.endTime),
+      startTime: inicio,
+      endTime: final,
       allDay: this.newEvent.allDay,
       author: this.usuario,
       sponsor: this.newEvent.sponsor,
@@ -202,30 +234,85 @@ export class HomePage {
       description: this.newEvent.description,
       notify: this.newEvent.notify
     }
-    this.eventsSource.push(toAdd)
-    this.myCal.loadEvents()
-    this.events.addData(toAdd)
-    this.newEvent = {
-      title: "",
-      startTime: null,
-      endTime: null,
-      allDay: false,
-      author: "",
-      sponsor: "",
-      notify: false
+    //SI ESTOY EDITANDO UN EVENTO...
+    if (this.isEditingEvent) {
+      for (let i = 0; i < this.eventsSource.length; i++) {
+        if (this.eventsSource[i].id == this.evento.id) {
+          toAdd.id = this.evento.id
+          this.eventsSource[i] = toAdd
+          await this.events.setData(this.eventsSource)
+          break
+        }
+      }
+      this.isEditingEvent = false
+      //SINO LO AÑADO A LA LISTA.
+    } else {
+      await this.events.addData(toAdd)
     }
+    this.eventsSource = await this.events.getData()
+    this.myCal.loadEvents()
+    this.zone.run(() => {
+      this.newEvent = {
+        id: -1,
+        title: "",
+        author: this.usuario,
+        startTime: null,
+        endTime: null,
+        allDay: false,
+        sponsor: "",
+        place: "",
+        description: "",
+        notify: false
+      }
+      this.myCal.currentDate = inicio
+    })
     this.setOpen(false)
   }
+  
+  eliminarEvento(){
+    this.pregunta("¿Seguro de eliminar?", async ()=>{
+      for (let i = 0; i < this.eventsSource.length; i++) {
+        if (this.eventsSource[i].id == this.evento.id) {
+
+          this.eventsSource.splice(i, 1)
+          await this.events.setData(this.eventsSource)
+          this.eventsSource = await this.events.getData()
+          this.myCal.loadEvents()
+          this.isEventOpen = false
+          break
+        }
+      }
+    })
+  }
+
+  cerrarModal() {
+    this.isModalOpen = false
+    this.cancelarClick()
+  }
+
+  async cambiaVista(valor: string) {
+    this.toggleOpcionesModo()
+    // await new Promise(resolve => setTimeout(resolve, 300));
+    this.calendar.mode = valor as CalendarMode
+    if (valor == 'day' || valor == 'week') {
+      document.querySelector(".eventos-border")?.classList.add("oculto")
+      document.querySelector(".eventos-border")?.classList.remove("visible")
+      document.querySelector(".calendar-container")?.classList.add("full")
+      document.querySelector(".calendar-container")?.classList.remove("middle")
+    } else {
+      document.querySelector(".eventos-border")?.classList.add("visible")
+      document.querySelector(".eventos-border")?.classList.remove("oculto")
+      document.querySelector(".calendar-container")?.classList.add("middle")
+      document.querySelector(".calendar-container")?.classList.remove("full")
+    }
+
+  }
+
+
 
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
-  }
-
-  checkear() {
-    console.log("Chekeando")
-    this.newEvent.allDay = !this.newEvent.allDay
-    console.log(this.newEvent)
   }
 
   onViewTitleChanged(evt: any) {
@@ -241,14 +328,13 @@ export class HomePage {
   }
 
   onTimeSelected(evt: { selectedTime: Date, events: any[], disabled: boolean }) {
-    //ESTO PROVOCA EL CAMBIO DE FECHA SELECCIONADA AL HACER CLIC
     this.fechaSeleccionada = format(evt.selectedTime, "MMMM dd 'de' yyyy", { locale: es })
     this.newEvent.startTime = format(evt.selectedTime, "yyyy-MM-dd'T'HH:mm:ss")
-    this.newEvent.endTime = format(evt.selectedTime.setHours(evt.selectedTime.getHours() + 1), "yyyy-MM-dd'T'HH:mm:ss")
-    // this.newEvent.endTime = evt.getHours() + 1
-    // this.newEvent.startTime = format(evt.selectedTime, "yyyy-MM-dd'T'HH:mm:ss",)
-    // const later = evt.selectedTime.setHours(evt.selectedTime.getHours() + 1)
-    // this.newEvent.endTime = format(later, "yyyy-MM-dd'T'HH:mm:ss")
+
+    let fin = new Date(this.newEvent.startTime)
+    fin.setHours(fin.getHours() + 1)
+    this.newEvent.endTime = format(fin, "yyyy-MM-dd'T'HH:mm:ss")
+
     this.eventos = evt.events.sort((n1, n2) => {
       if (n1.allDay) {
         return -1;
@@ -258,6 +344,7 @@ export class HomePage {
         return n1.startTime - n2.startTime;
       }
     });
+
 
     // if (evt.events.length > 0) {
     this.anim.create()
@@ -274,7 +361,10 @@ export class HomePage {
     // if (this.calendar.mode == 'day' || this.calendar.mode == 'week') {
     //   this.setOpen(true)
     // }
-    this.cdr.detectChanges();
+    // if (this.calendar.mode == 'month') {
+      this.cdr.detectChanges();
+    // }
+
   }
 
   // Focus today
@@ -352,7 +442,9 @@ export class HomePage {
         document.documentElement.style.setProperty('--texto-input', '#424242');
         document.documentElement.style.setProperty('--texto-nombre-dias', '#8e8e8e');
         document.documentElement.style.setProperty('--fondo-footer', '#1C1F32');
-
+        document.documentElement.style.setProperty('--texto-eventos-calendario', '#fff');
+        document.documentElement.style.setProperty('--color-grilla', '#dddddd11');
+        document.documentElement.style.setProperty('--color-text-muted', '#bababa');
       } else {
         console.log("Aplicando tema claro")
         document.documentElement.style.setProperty('--fondo-calendario', '#E8E8E8');
@@ -365,6 +457,9 @@ export class HomePage {
         document.documentElement.style.setProperty('--fondo-input', '#fff');
         document.documentElement.style.setProperty('--texto-input', '#424242');
         document.documentElement.style.setProperty('--fondo-footer', '#F1F1F1');
+        document.documentElement.style.setProperty('--texto-eventos-calendario', '#000');
+        document.documentElement.style.setProperty('--color-grilla', '#00000011');
+        document.documentElement.style.setProperty('--color-text-muted', '#848484');
       }
     })
 
@@ -415,5 +510,34 @@ export class HomePage {
       cssClass: 'custom-toast'
     });
     await toast.present();
+  }
+
+  editarClick(evento: any) {
+    this.newEvent.id = evento.id
+    this.isEditingEvent = true
+    this.newEvent.title = evento.title
+    this.newEvent.allDay = evento.allDay
+    this.newEvent.startTime = format(evento.startTime, "yyyy-MM-dd'T'HH:mm:ss")
+    this.newEvent.endTime = format(evento.endTime, "yyyy-MM-dd'T'HH:mm:ss")
+    this.newEvent.sponsor = evento.sponsor
+    this.newEvent.place = evento.place
+    this.newEvent.description = evento.description
+    this.isEventOpen = false
+    this.setOpen(true)
+  }
+
+  cancelarClick() {
+    if (this.isEditingEvent) {
+      this.newEvent.id = -1
+      this.newEvent.title = ""
+      this.newEvent.allDay = false
+      this.newEvent.startTime = format(new Date(), "yyyy-MM-dd'T'HH:mm:ss")
+      this.newEvent.endTime = format(new Date(), "yyyy-MM-dd'T'HH:mm:ss")
+      this.newEvent.sponsor = ""
+      this.newEvent.place = ""
+      this.newEvent.description = ""
+      this.isEditingEvent = false
+    }
+
   }
 }
